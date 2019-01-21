20d81b98acaa6f243fc8d9147e860689737a1dea
gingi99 <bigtree3101@gmail.com>
2019-01-21 21:11:47 +0900

    update init install.packages

---
+++ init_install_packages.R
@@ -0,0 +1,27 @@
+# Tidyvese
+install.packages("tidyverse")
+
+# Devtools
+install.packages("devtools")
+
+# Data Load
+install.packages("data.table")
+
+# ML
+devtools::install_github("yanyachen/MLmetrics")
+
+# Plot
+install.packages("pheatmap")
+install.packages("corrplot")
+
+# EDA
+install.packages("skimr")
+install.packages("janitor")
+install.packages("DataExplorer")
+install.packages("summarytools")
+
+# Database 
+install.packages("googlesheets")
+
+# Timeserise Analysis
+install.packages("RcppRoll")
