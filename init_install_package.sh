57b5dedb32f4bd433218c37b8ca2f37e960ada0a
gingi99 <bigtree3101@gmail.com>
2019-02-26 13:15:27 +0900

    update apt install libpoppler-cpp-dev

--- init_install_package.sh
+++ init_install_package.sh
@@ -2,3 +2,4 @@ sudo apt install libcurl4-openssl-dev libxml2-dev libssl-dev # for basic package
 sudo apt install build-essential cmake # for LightGBM
 sudo apt install libudunits2-dev # for ggforce
 sudo apt install libmagick++-dev # for LIME
+sudo apt install libpoppler-cpp-dev # for pdftool
