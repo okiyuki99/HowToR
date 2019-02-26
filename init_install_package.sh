aa660319b32c9ebf1f3e2cdd4f71b7c6be89cfb7
gingi99 <bigtree3101@gmail.com>
2019-02-26 13:05:58 +0900

    update apt install libmagick++-dev

--- init_install_package.sh
+++ init_install_package.sh
@@ -1,3 +1,4 @@
 sudo apt install libcurl4-openssl-dev libxml2-dev libssl-dev # for basic packages
 sudo apt install build-essential cmake # for LightGBM
 sudo apt install libudunits2-dev # for ggforce
+sudo apt install libmagick++-dev # for LIME
