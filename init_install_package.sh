2fa4d36f45935c184f24b080b11b25ee67b8753a
gingi99 <bigtree3101@gmail.com>
2019-03-17 20:40:37 +0900

    update init_install

--- init_install_package.sh
+++ init_install_package.sh
@@ -3,3 +3,4 @@ sudo apt install build-essential cmake # for LightGBM
 sudo apt install libudunits2-dev # for ggforce
 sudo apt install libmagick++-dev # for LIME
 sudo apt install libpoppler-cpp-dev # for pdftool
+sudo apt install libgdal-dev # for DALEX
